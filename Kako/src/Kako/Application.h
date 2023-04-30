6d6682b25b7c0913187497695e010c5da3832801
kayiko <1643030440@qq.com>
2023-04-30 21:49:01 +0800

    Add Window Event

--- Kako/src/Kako/Application.h
+++ Kako/src/Kako/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include"Events/Event.h"
+#include "Kako/Events/ApplicationEvent.h"
 #include "Window.h"
 namespace Kako {
     class HAZEL_API Application
@@ -9,7 +10,9 @@ namespace Kako {
             Application();
             virtual~Application();
             void Run();
+            void OnEvent(Event& e);
         private:
+            bool OnWindowClose(WindowCloseEvent& e);
             std::unique_ptr<Window> m_Window;
             bool m_Running = true;
     };
